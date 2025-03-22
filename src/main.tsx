8faf2f79fe8ba313cf002c57b718f28785c8f074
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2025-03-22 00:30:49 -0300

    create start project

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import { RegistryProvider } from './registry/RegistryProvider.ts';
-import { AccountGatewayHttp } from './gateway/AccountGateway.ts';
-// import FetchAdapter from './http/FetchAdapter.ts';
-import AxiosAdapter from './http/AxiosAdapter.ts';
-
-const httpClient = new AxiosAdapter();
-// const httpClient = new FetchAdapter();
-const accountGateway = new AccountGatewayHttp(httpClient);
-
-const registry = {
-  accountGateway
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RegistryProvider value={registry}>
-      <App />
-    </RegistryProvider>
+    <App />
   </StrictMode>,
 )
